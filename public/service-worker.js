c768892a852a3ca1c3594126988d1e7858693513
Ghidorah <jljohnson1454@gmail.com>
2021-12-19 14:26:56 -0600

    minor changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,13 @@ const FILES_TO_CACHE = [
     "./js/index.js"
 ]
 
-
+if ('serviceWorker' in navigator) {
+    caches.keys().then(function(cacheNames) {
+      cacheNames.forEach(function(cacheName) {
+        caches.delete(cacheName);
+      });
+    });
+  }
 
 
 
